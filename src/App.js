b94829a07089e94ec5f6729787561ec082e1d22f
drisbhi <76056109+drisbhi@users.noreply.github.com>
2021-04-19 21:39:43 +0530

    first changes

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h1>Sahib bAJAJ</h1>
+      <h2>FINALLY DONE </h2>
     </div>
   );
 }
